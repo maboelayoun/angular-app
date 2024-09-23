b003d417f3fce2e7098db0219a6df6e1166381de
Mm@@01147780927 <maboelayoun2@gmail.com>
2024-09-23 19:50:41 +0300

    pass data from parent to child and from child to parent

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, computed, EventEmitter, input, Input, Output, output } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 @Component({
@@ -9,8 +9,16 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
+@Input({required:true})id!:string;
 @Input({required:true}) avatar!:string;
 @Input({required:true}) name!:string;
+@Output()select=new EventEmitter();
+
+// avatar=input.required<string>();
+// name=input.required<string>();
+// pathImage=computed(()=>{
+//   return   "assets/users/"+this.avatar();
+// })
 get pathImage(){
   return "assets/users/"+this.avatar;
 }
@@ -18,6 +26,7 @@ get selectedUser(){
   return this.name;
 }
 onSelectUser(){
+  this.select.emit(this.id);
   
 }
 }
